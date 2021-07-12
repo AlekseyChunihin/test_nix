4b14a49019bfcc0e2d86b5f7d08b3c756dd0decf
Aleksey Chunihin <alexey3322179@gmail.com>
2021-07-12 22:14:29 +0300

    add new class

--- unit_1_nixTest/src/main/java/org/example/Student.java
+++ unit_1_nixTest/src/main/java/org/example/Student.java
@@ -12,6 +12,14 @@ public class Student {
         this.name = name;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     @Override
     public String toString() {
         return "Student{" +
